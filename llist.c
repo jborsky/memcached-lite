787ea22125ba75edc0b94c2f4012f013f6589a64
J4M3S_CZ <jakub.borsky@gmail.com>
2024-03-12 19:16:02 +0100

    Implemented llist

---
+++ llist.c
@@ -0,0 +1,76 @@
+#include "llist.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+
+struct llist *llist_init(void)
+{
+    struct llist *new_list = malloc(sizeof(*new_list));
+    if (new_list == NULL)
+        return NULL;
+
+    new_list->head = NULL;
+
+    return new_list;
+}
+
+bool llist_insert(struct llist *list, const char *key, int nbytes, void *data)
+{
+    struct node *new_node = malloc(sizeof(*new_node));
+    if (new_node == NULL)
+        return false;
+
+    new_node->key = malloc(nbytes);
+    if (new_node->key == NULL) {
+        free(new_node);
+        return false;
+    }
+    memcpy(new_node->key, key, nbytes);
+
+    new_node->data = data;
+    new_node->next = list->head;
+    list->head = new_node;
+
+    return true;
+}
+
+struct node *llist_search(struct llist *list, const char *key, int nbytes)
+{
+    for (struct node *node = list->head; node != NULL; node = node->next) {
+        if ((nbytes == node->nbytes) && memcmp(node->key, key, nbytes) == 0)
+            return node;
+    }
+
+    return NULL;
+}
+
+struct node *llist_delete(struct llist *list, const char *key, int nbytes)
+{
+    struct node **node;
+    for (node = &list->head; *node != NULL; node = &(*node)->next) {
+        if ((nbytes == (*node)->nbytes) && memcmp((*node)->key, key, nbytes) == 0)
+            break;
+    }
+
+    if (*node == NULL)
+        return NULL;
+
+    struct node *deleted_node = *node;
+
+    *node = (*node)->next;
+
+    return deleted_node;
+}
+
+void llist_destroy(struct llist *list)
+{
+    for (struct node *node = list->head; node != NULL;) {
+        struct node *next_node = node->next;
+        free(node->key);
+        free(node);
+        node = next_node;
+    }
+
+    free(list);
+}
---
+++ llist.h
@@ -0,0 +1,26 @@
+#ifndef LLIST_H
+#define LLIST_H
+
+#include <stdbool.h>
+
+struct node
+{
+    char *key;
+    int nbytes;
+    void *data;
+    struct node *next;
+};
+
+struct llist
+{
+    struct node *head;
+};
+
+struct llist *llist_init(void);
+bool llist_insert(struct llist *list, const char *key, int nbytes, void *data);
+struct node *llist_search(struct llist *list, const char *key, int nbytes);
+struct node *llist_delete(struct llist *list, const char *key, int nbytes);
+void llist_destroy(struct llist *list);
+
+
+#endif //LLIST_H
