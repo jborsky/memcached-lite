5a381f4aac5c713da6b2cf9836353dd311507803
J4M3S_CZ <jakub.borsky@gmail.com>
2024-04-25 21:32:47 +0200

    Move client handling logic to client handler

--- server.c
+++ server.c
@@ -6,10 +6,9 @@
 #include <poll.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <fcntl.h>
 #include "server.h"
-#include "request.h"
+#include "client_handler.h"
 
 int bind_and_listen(const char *ip, int port)
 {
@@ -78,41 +77,6 @@ int increase_capacity(struct server *server)
     return 0;
 }
 
-void destroy_server(struct server *server)
-{
-    close(server->fd);
-    free(server->clients);
-    free(server->pfds);
-}
-
-int buffer_append(struct buffer *buff, const char *data, size_t nbytes)
-{
-    if (nbytes >= buff->size - buff->count) {
-        if (buff->buff == NULL)
-            buff->size = 256;
-
-        char *tmp = realloc(buff->buff, buff->size * 2 * sizeof(*tmp));
-        if (tmp == NULL)
-            return -1;
-
-        buff->buff = tmp;
-        buff->size *= 2;
-    }
-
-    memcpy(buff->buff + buff->count, data, nbytes);
-    buff->count += nbytes;
-
-    return 0;
-}
-
-void buffer_clear(struct buffer *buff)
-{
-    free(buff->buff);
-    buff->buff = NULL;
-    buff->size = 0;
-    buff->count = 0;
-}
-
 void remove_client(struct server *server, int index)
 {
     if (index >= server->size || index < 0)
@@ -123,13 +87,7 @@ void remove_client(struct server *server, int index)
 
     server->pfds[index].fd = -1;
 
-    struct client *client = &server->clients[index];
-    client->fd = -1;
-    buffer_clear(&client->in);
-    buffer_clear(&client->out);
-    buffer_clear(&client->out_data);
-    free_request(client->req);
-    client->req = NULL;
+    free_client(&server->clients[index]);
 }
 
 void accept_client(struct server *server)
@@ -158,64 +116,13 @@ void accept_client(struct server *server)
     server->clients[index].fd = client_fd;
     server->pfds[index].events = POLLIN | POLLOUT;
 
-    const char *welcome = "Connected\r\n";
-    if (buffer_append(&server->clients[index].out, welcome, strlen(welcome) == -1))
+    if (response_to_client(&server->clients[index], "Connected\r\n") == -1)
         goto error;
 
     return;
 
 error:
     remove_client(server, index);
-}
-
-int read_message(struct client *client) {
-    char buffer[512];
-
-    int bytes_read = read(client->fd, buffer, 512);
-    if (bytes_read <= 0)
-        return -1;
-
-    if (buffer_append(&client->in, &buffer, bytes_read) == -1)
-        return -1;
-
-    if (handle_client(client) == -1)
-        return -1;
-
-    return 0;
-}
-
-int send_data(struct client *client)
-{
-    if (client->out_data.buff == NULL)
-        return 0;
-
-    struct buffer *buff = &client->out_data;
-
-    ssize_t bytes_written = write(client->fd, buff->buff + buff->count, buff->size - buff->count);
-    if (bytes_written == -1)
-        return -1;
-
-    buff->count += bytes_written;
-
-    if (buff->count == buff->size)
-        buff->buff = NULL;
-
-    return 0;
-}
-
-int send_message(struct client *client)
-{
-    if (client->out.count == 0)
-        return send_data(client);
-
-    ssize_t bytes_written = write(client->fd, client->out.buff, client->out.count);
-    if (bytes_written == -1)
-        return -1;
-
-    client->out.count -= bytes_written;
-    memmove(client->out.buff, client->out.buff + bytes_written, client->out.count);
-
-    return 0;
 }
 
 int memcached_server(int sock_fd)
@@ -230,24 +137,25 @@ int memcached_server(int sock_fd)
 
         for (int i = 0; i < server.size; ++i) {
             if (server.pfds[i].revents & (POLLIN | POLLHUP)) {
-                if (server.pfds[i].fd != server.fd) {
-                    int ret = read_message(&server.clients[i]);
-                    if (ret == -1)
-                        remove_client(&server, i);
-                }
-
-                else {
+                if (server.pfds[i].fd == server.fd)
                     accept_client(&server);
-                }
-            }
 
-            if (server.pfds[i].revents & POLLOUT) {
-                int ret = send_message(&server.clients[i]);
-                if (ret == -1)
+                if (handle_client_in(&server.clients[i]) == -1)
                     remove_client(&server, i);
             }
+
+            if (server.pfds[i].revents & POLLOUT)
+                if (handle_client_out(&server.clients[i]) == -1)
+                    remove_client(&server, i);
         }
     }
+}
+
+void destroy_server(struct server *server)
+{
+    close(server->fd);
+    free(server->clients);
+    free(server->pfds);
 }
 
 
